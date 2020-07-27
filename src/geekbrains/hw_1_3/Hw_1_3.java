831ee2c289e1ee3f07c434b46f98b6ac050b57c5
Chobotock <ilya_ageev@bk.ru>
2020-07-27 14:16:34 +0300

    Урок 3. Java. Практика

--- src/geekbrains/hw_1_3/Hw_1_3.java
+++ src/geekbrains/hw_1_3/Hw_1_3.java
@@ -12,7 +12,36 @@ public class Hw_1_3 {
 
     public static void main(String[] args) {
 
+        int userVar;
 
+        do {                                                        // делаем циклы, пока пользователь не выберет 0 по условию while;
+            Random rand = new Random();
+            int randNumber = rand.nextInt(10);                // рандомайз числа от 0 до 9;
+
+            for (int i = 0; i < 3; i++) {                           // циклы, работающие с введенным пользователем числом и ограниченные 3 попытками (3 цикла);
+                Scanner sc = new Scanner(System.in);                // спрашиваем число у пользователя и просим ввести;
+                System.out.println("Введите число от 0 до 9: ");
+                int userNumber = sc.nextInt();
+
+                if (userNumber == randNumber) {                     // проходим по условиям;
+                    System.out.println("Поздравляю! Вы угадали!");
+                    break;
+                } else if (userNumber > randNumber) {
+                    System.out.println("Вы ввели слишком большое число");
+                } else {
+                    System.out.println("Вы ввели слишком маленькое число");
+                }
+
+                if (i == 2) {                                       // когда исчерпали попытки - сообщаем о проигрыше;
+                    System.out.println("Вы проиграли");
+                }
+            }
+
+            Scanner sc = new Scanner(System.in);                                // после выхода из цикла for спрашиваем о желании продолжить или прекратить игру и записываем ответ пользователя;
+            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
+            userVar = sc.nextInt();
+
+        } while (userVar == 1);                                     // сравниваем с ответом пользователя. Если 1, то повторяем while;
     }
 }
 
